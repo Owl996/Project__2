422ed454b35a2b4f01c17ad6971b01cef6416b8f
Denis <dlironov@bk.ru>
2021-12-20 01:59:47 +0700

    Добавьте файлы проекта.

---
+++ Application2/Application2.cpp
@@ -0,0 +1,18 @@
+﻿// Application2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+std::string task_3(const std::string& s1, const std::string& s2) {
+    return s1 + s2;
+}
+
+int main()
+{
+    std::string str1 = "Hello", str2 = " World";
+
+    std::cout << task_3(str1, str2) << std::endl;
+
+    system("pause");
+    return 0;
+}
